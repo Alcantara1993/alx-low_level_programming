1518241b895edcb45535d77678b5fdc5d110266c
Qawiyyi Uthman <uthman2senior@gmail.com>
2022-06-03 13:49:00 -0700

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *   * main - Prints a text
  *     *
  *       * Return: Always 0 (Success)
- *         */
+ *
+         */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
