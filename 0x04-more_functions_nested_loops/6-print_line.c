e71c2fafab0d4ef0b5f5221ffa26d9f5c903a495
Qawiyyi Uthman <uthman2senior@gmail.com>
2022-06-09 12:41:59 -0700

    Commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_line - Draws a straight line using the character _.
+*Draws a straight line using the character _.
 * @n: The number of _ characters to be printed.
 */
 
