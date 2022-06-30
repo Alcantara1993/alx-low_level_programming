204fb5799620d917229d619e17283fc87c6eab33
Qawiyyi Uthman <uthman2senior@gmail.com>
2022-06-30 09:27:42 -0700

    Commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,7 +6,7 @@
  *   * @b: The number of bytes to be allocated.
  *    *
  *     * Return: A pointer to the allocated memory.
- *      */
+*/
 void *malloc_checked(unsigned int b)
 {
 	void *mem = malloc(b);
