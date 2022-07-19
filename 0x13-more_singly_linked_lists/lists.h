2d6e26c22535c5642d1d781ce419e5edcf5f0b4d
Qawiyyi Uthman <uthman2senior@gmail.com>
2022-07-19 10:40:17 -0700

    Commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,9 +2,9 @@
 #define LISTS_H
 
 /*
- *  * File: lists.h
- *   * Desc: Header file containing prototypes and definitions for all functions
- *    *       and types written in the 0x12-more_singly_linked_lists directory.
+*  * File: lists.h
+* Desc: Header file containing prototypes and definitions for all functions
+*       and types written in the 0x12-more_singly_linked_lists directory.
 */
 
 #include <stdlib.h>
