87ea74133b4d2ca50334b191ca0e93cae3c10bd3
Qawiyyi Uthman <uthman2senior@gmail.com>
2022-07-21 12:38:12 -0700

    Commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
-* binary_to_uint - converts a binary number to an unsigned int.
-* @b: pointer to a string containing a binary number
-*
-* signed int with decimal value of binsry number, or 0 if error
+ *  * binary_to_uint - converts a binary number to an unsigned int.
+ *   * @b: pointer to a string containing a binary number
+ *    *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
 */
 unsigned int binary_to_uint(const char *b)
 {
